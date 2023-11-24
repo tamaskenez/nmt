77dc7ee50b7245c59234d7f9b5c5664ef1eaf6d0
Tamas Kenez <tamas.kenez@gmail.com>
2023-11-25 00:20:59 +0100

    remove test.cpp

--- 3rdparty/dspinellis_tokenizer/test.cpp
+++
@@ -1,6 +0,0 @@
-// First line comment
-std::vector<int> a;
-std::vector<std::vector<int>> /* here comes b */ b;
-int c = 1 << 3;  // Single line comment
-int d = 1 >> 3;
-const char* s = "string literal\t\n";
