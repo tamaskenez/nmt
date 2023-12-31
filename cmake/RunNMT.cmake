7274c0cc1fc56cca97010ec7c6346a0dfda6cea8
Tamas Kenez <tamas.kenez@gmail.com>
2023-12-31 21:22:18 +0100

    update RunNMT with new policies

--- cmake/RunNMT.cmake
+++ cmake/RunNMT.cmake
@@ -1,25 +1,40 @@
 function(run_nmt target)
 	cmake_parse_arguments(PARSE_ARGV 1 ARG
 		""
-		""
-		"FILES")
-	set(sources_input_file_path "${CMAKE_CURRENT_BINARY_DIR}/generated/sources_input.txt")
+		"SOURCE_DIR"
+		"")
+	if(NOT ARG_SOURCE_DIR)
+		message(FATAL_ERROR "Missing parameter: SOURCE_DIR")
+	endif()
+
+	# Default base directory is CMAKE_CURRENT_SOURCE_DIR.
+	cmake_path(ABSOLUTE_PATH ARG_SOURCE_DIR NORMALIZE)
+
+	# Perform GLOB_RECURSE to trigger config if anything changes
+	# in the source directory.
+	file(GLOB_RECURSE sources CONFIGURE_DEPENDS *)
+
 	set(output_dir "${CMAKE_CURRENT_BINARY_DIR}/generated/nmt")
-	list(JOIN ARG_FILES "\n" content)
-	file(WRITE ${sources_input_file_path} ${content} "\n")
 	execute_process(COMMAND ${NMT_PROGRAM}
-		--sources ${sources_input_file_path}
+		--source-dir ${ARG_SOURCE_DIR}
+		--target ${target}
 		--output-dir ${output_dir}
 		COMMAND_ECHO STDOUT
 		COMMAND_ERROR_IS_FATAL ANY
 	)
+
+	# Read output file list from running `nmt`.
 	file(STRINGS ${output_dir}/files.txt generated_files)
+
+	# Diagnostics; print the contents of `files.txt`
 	set(generated_files_rel "")
 	foreach(f IN LISTS generated_files)
  		cmake_path(RELATIVE_PATH f BASE_DIRECTORY ${output_dir})
  		set(generated_files_rel "${generated_files_rel}${f} ")
  	endforeach()
 	message(STATUS "reading file list ${output_dir}/files.txt -> ${generated_files_rel}")
+
+	# Add the file list and the generated source files to the target.
 	source_group(boilerplate FILES ${output_dir}/files.txt ${generated_files})
 	target_sources(${target} PRIVATE ${output_dir}/files.txt ${generated_files})
 	target_include_directories(${target}
@@ -30,8 +45,9 @@ function(run_nmt target)
 	add_custom_command(TARGET ${target}
 	                   PRE_BUILD
 	                   COMMAND ${NMT_PROGRAM}
-	            	   		--sources ${sources_input_file_path}
-							--output-dir ${output_dir}
+	                   --source-dir ${ARG_SOURCE_DIR}
+	                   --target ${target}
+	                   --output-dir ${output_dir}
 	                   BYPRODUCTS ${output_dir}/files.txt
 	                   COMMENT "Running nmt."
 	)
